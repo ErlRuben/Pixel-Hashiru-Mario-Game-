8eec93a4f3215b046028ddc1a3e871c0764ab380
lelazeb123 <bezalelrobin@gmail.com>
2020-02-29 14:21:54 +0800

    Working Audio

--- js/levels/Scene1.js
+++ js/levels/Scene1.js
@@ -9,10 +9,17 @@ class Scene1 extends Phaser.Scene {
     this.load.image('title', 'assets/images/title.png');
     this.load.image('start', 'assets/images/start.png');
 
+    this.load.audio('title music', 'assets/audio/mainmenu.mp3');
+
   }
   create() {
     this.add.image(720, 300, 'bg');
     this.add.image(720, 300, 'title');
+
+    this.sound.pauseOnBlur = false;
+    this.sound.play('title music', {
+      loop: true
+    })
 
     const helloButton = this.add.image(710, 500, 'start');
     helloButton.setInteractive();
